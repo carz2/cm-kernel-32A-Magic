0059e01ac99193d24d6ca13e61f98fa804228328
carz2 <carz8500@gmail.com>
2010-08-05 23:57:06 -0600

    add amss 6355 support

--- arch/arm/mach-msm/msm_vibrator.c
+++ arch/arm/mach-msm/msm_vibrator.c
@@ -21,6 +21,7 @@
 #include <linux/sched.h>
 
 #include <mach/msm_rpcrouter.h>
+#include <mach/msm_rpc_version.h>
 
 #define PM_LIBPROG	  0x30000061
 #if (CONFIG_MSM_AMSS_VERSION == 6220) || (CONFIG_MSM_AMSS_VERSION == 6225)
